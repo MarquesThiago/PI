267f9b7cf953b0a55997280a0c18857fb8df2f6a
MarquesThiago <48632838+MarquesThiago@users.noreply.github.com>
2019-12-05 00:06:30 -0300

    add content
    create text of content of the div of page home,, create link to gallery e add fotos from site

--- site/src/pages/home/home.js
+++ site/src/pages/home/home.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../../style/home.css"
+import {Link} from "react-router-dom"
 // import {id, className } from "postcss-selector-parser"
 
 export default function Home(){
@@ -24,7 +25,9 @@ export default function Home(){
 
         <>
             <div className="content-main" >
+
                 <h1> Bem-vindo ao Reciclar Cores </h1>
+
                 <h2> Agradeçemos sua presença</h2>
 
                 <p> A todos gostariamos de agradecer por estarem presentes aqui hoje e esperamos 
@@ -32,6 +35,7 @@ export default function Home(){
                     Projecto Integrador. Mas que isso queremos que poder agregar na vida de cada,  
                     conhecimentos sobre esse tema tão importante que é a sustentabilidade. 
                 </p>
+
             </div>
             <button className="link firts-action"  onClick = { event => {rep('firts')}} >
                  <h2>Reciclagem Senac</h2>
@@ -40,18 +44,47 @@ export default function Home(){
             </button>
 
             <div className="link-content" id="firts">
-                
+
+                <h2 className="home-menu-ttl">Palestras com os Funcionarios da Limpeza do SENAC</h2>
+
+                <p className="home-menu-txt">A palestra no SENAC foi elaborada para passar alguns 
+                dos conhecimentos obtidos durante nossa investigação sobre o tema gerar do Projeto 
+                (sustentabilidade: Reciclagem), o que por coincidência os funcionários da limpeza do 
+                Senac também estava realizam uma trabalho sobre a separação de lixo.</p>
+
+                <p className="home-menu-txt">Estruturamos uma apresentação que introduzisse 
+                algumas das orientações sobre o tema. O resultado desta ação não poderia ser mais 
+               inesperado, pois durante a realização obtivemos grandes trocas de 
+                informações. conhecimento que atriubuiram nas realização das ações posterioes.</p>
+
+
+
+
+                <dvi className = "view_otherpg">
+                    <p className ="text-pre-btn">Clique abaixo  para visualiza a galeria de fotos</p>
+                    <br/>
+
+                    <Link to="/palestra_senac/photos">
+
+                        <button className="btn">Galeria</button>
+                    </Link>
+                </dvi>
+
+
             </div>
 
             
 
             <button className="link second-action" onClick = { event => {rep('second')}}>
                 <h2>Dia da Grande Ação</h2>
-                
             </button>
 
             <div className="link-content" id='second'>
-                
+
+                <h2 className="home-menu-ttl"> Dia da Grande Ação</h2>
+
+                <p className="home-menu-txt">O nosso Projeto Integrador foi gerado pensando em algo que fosse interativo e pudesse mudar as coisas realmente. Apartir dai decidimos trabalhar com crianças pois temos a conciência que elas sã o o futuro e que realmente que mvai fazer qa diferença são elas.</p>
+                <p className="home-menu-txt">A grande ação foi realizada em uma escola Pirituba é não poderia ter resuldado melhor, indiferente as dificuldades</p>
             </div>
 
 
@@ -60,8 +93,20 @@ export default function Home(){
             </button>
 
             <div className="link-content" id= "third">
-                <h3>Apresentação das informações apresentadas por representante da empresa LOGA</h3>
-                <p> A LOGA e a empresa logistica uma das duas empresas responsaveis  pela coleta de lixo no Estado de São Paulo.</p>
+            
+                <h2 className="home-menu-ttl">Apresentação das informações apresentadas por 
+                representante da empresa LOGA </h2>
+
+                <p className="home-menu-txt" >Com a colaboração de uma das funcionárias da empresa 
+                LOGA, uma das empresas responsáveis pela a logística de recolhimento de resíduos do 
+                Estado de São Paulo. Podemos nos aprofundar mais no conhecimento de um tema que não 
+                e tão discutido: “o lixo para onde vai? ”, esse foi o tema ao qual girou 
+                a palestra. </p>
+
+                <p className="home-menu-txt">Também tivemos o prazer de contar com a de outra turma do 
+                SENAC, que tinha também o tema gerador sustentabilidade, e junto com eles esclarecer
+                inúmeras dúvidas.</p>
+
             </div>
             
         </>
--- site/src/router.js
+++ site/src/router.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Home from "./pages/home/home"
+import Senac_Galeria from "./pages/pales_senac/galeria/galeria"
 
 export default function Router(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/> 
+                <Route path="/" exact component={Home}/>
+                <Route path="/palestra_senac/photos" component={Senac_Galeria}/>
                 
             </Switch>
         </BrowserRouter>
